af442e664ef436f8afca376a461516e45cb914e5
Emnet19 <162457277+Emnet19@users.noreply.github.com>
2025-03-07 15:08:55 +0300

    Create App.jsx

---
+++ Task1/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UseCard from "./UseCard";
+const App = () => {
+  return (
+    <div style={{ display: "flex", gap: "20px", justifyContent: "center", marginTop: "50px" }}> 
+      <UseCard name="John Doe" email="john@example.com" age={28} bgColor="#FFDDC1" />
+      <UseCard   name="Jane Smith" email="jane@example.com" age={25} bgColor="#C1E1FF" />
+      <UseCard name="Alex Brown" email="alex@example.com" age={30} bgColor="#C1FFC1" />
+    </div>
+  );
+};
+
+export default App;
