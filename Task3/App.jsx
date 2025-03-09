612721413e06003a9b911302e277315f69898cfe
Emnet19 <162457277+Emnet19@users.noreply.github.com>
2025-03-09 13:09:36 +0300

    Create App.jsx

---
+++ Task3/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TaskManager from './TaskManager'
+function App() {
+  return (
+    <div>
+      <TaskManager></TaskManager>
+    </div>
+  )
+}
+
+export default App
