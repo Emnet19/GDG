80a5fdb8ff9948b26b777a7724fefca82fc8a5a5
Emnet19 <162457277+Emnet19@users.noreply.github.com>
2025-03-09 13:44:42 +0300

    Create ContactUs.jsx

---
+++ Task4/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Contact(){
+    return<>
+      <div>
+        <h2> Contact Page</h2>
+        <p>To contact us use our email </p>
+        <p>Email: example@gmail.com</p>
+      </div>
+    
+    </>
+}
+export default Contact
