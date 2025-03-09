ab1d0ef7bd5bcb52c1cdb3a3f9c703569e1ac666
Emnet19 <162457277+Emnet19@users.noreply.github.com>
2025-03-09 13:43:53 +0300

    Create AboutUs.jsx

---
+++ Task4/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+       <p>
+        This is the about page of our app
+       </p>
+    </div>
+  )
+}
+
+export default AboutUs
